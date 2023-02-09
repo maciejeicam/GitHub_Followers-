d6bf9550d0336bb9b831acee3bfc9e4f8084b40e
maciejeicam <maciej_michalik@onet.pl>
2023-02-09 15:38:13 +0100

    create networkManager with error messages

---
+++ GitHubFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GitHubFollowers
+//
+//  Created by Maciej Michalik on 09/02/2023.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. PLease try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse = "Invalid response from the server. PLease try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
