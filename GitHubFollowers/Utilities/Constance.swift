f6d1f413add8badba55cb8aa432c69881a25f55e
maciejeicam <maciej_michalik@onet.pl>
2023-02-22 16:55:58 +0100

    create second label, user info and constance for sf symbols

---
+++ GitHubFollowers/Utilities/Constance.swift
@@ -0,0 +1,12 @@
+//
+//  Constance.swift
+//  GitHubFollowers
+//
+//  Created by Maciej Michalik on 21/02/2023.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
